6325ecb8a62a465c40ea7ffd3518476a43474862
VSchnapka <133697445+VSchnapka@users.noreply.github.com>
2023-05-15 22:00:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ version = re.search(
     ).group(1)
 
 
-with open("README.rst", "rb") as f:
+with open("README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
     
     
 setup(
-    name = "modelfree-amide15N-lmft",
+    name = "modelfree-protein15n",
     packages = ["modfree"],
     entry_points = {
         "console_scripts": ['modfree = modfree.modfree:main']
@@ -22,8 +22,10 @@ setup(
     version = version,
     description = "Model free analysis of protein backbone amide 15N spin relaxation rates.",
     long_description = long_descr,
+    long_decription_content_type="test/markdown",
     author = "Vincent Schnapka",
     author_email = "vincentschnapka@gmail.com",
+    url="https://github.com/VSchnapka/modelfree-protein15n.git",
     install_requires=[
         'numpy',
         'scipy',
@@ -32,4 +34,12 @@ setup(
         'rich',
         'tomli',
     ]
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        ],
+    extras_requires = {
+        "dev": [
+            "pytest>=3.7",
+            ],
+        },
     )
