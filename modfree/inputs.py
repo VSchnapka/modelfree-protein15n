47a98ecfe018c5dabde3cf6e2ea9530b1f7267f5
Vincent Schnapka <133697445+VSchnapka@users.noreply.github.com>
2023-05-30 23:03:37 +0200

    Update inputs.py

--- modfree/inputs.py
+++ modfree/inputs.py
@@ -13,41 +13,33 @@ def read_relaxation_data_file(filedir):
     return x, y, dy
 
 
-def read_directory_file(filename="directories.toml", model="std"):
+def read_directory_file(filename="directories.toml"):
     rates = "R1", "R2", "NOE", "etaXY", "etaZ"
-    if model != "std":
-        print("in read directory file function: models other than std not yet implemented")
-        return None
-    if model == "std":
-        with open(filename, "rb") as f:
-            toml_dict = tomli.load(f)
-        dirs = dict()
-        for k in toml_dict.keys():
-            files = glob.glob(toml_dict[k]['directory']+"/*")
-            for f in files:
-                rate = re.findall('r1|r2|noe|etaxy|etaz', f, re.IGNORECASE)
-                rate = '' if rate == [] else rate[0]
-                for e in rates:
-                    match = re.search(rate, e, re.IGNORECASE)
-                    if match and match.group(0) != '':
-                        if toml_dict[k]['magnetic_field_MHz'] not in dirs.keys():
-                            dirs[toml_dict[k]['magnetic_field_MHz']] = dict()
-                        dirs[toml_dict[k]['magnetic_field_MHz']][e] = f
+    with open(filename, "rb") as f:
+        toml_dict = tomli.load(f)
+    dirs = dict()
+    for k in toml_dict.keys():
+        files = glob.glob(toml_dict[k]['directory']+"/*")
+        for f in files:
+            rate = re.findall('r1|r2|noe|etaxy|etaz', f, re.IGNORECASE)
+            rate = '' if rate == [] else rate[0]
+            for e in rates:
+                match = re.search(rate, e, re.IGNORECASE)
+                if match and match.group(0) != '':
+                    if toml_dict[k]['magnetic_field_MHz'] not in dirs.keys():
+                        dirs[toml_dict[k]['magnetic_field_MHz']] = dict()
+                    dirs[toml_dict[k]['magnetic_field_MHz']][e] = f
     return dirs
 
 
-def read_parameter_file(param_file, model="std"):
-    if model != "std":
-        print("in read directory file function: models other than std not yet implemented")
-        return None
-    if model == "std":
-        with open(param_file, "rb") as f:
-            toml_dict = tomli.load(f)
-        parameters = {"modes": toml_dict["modes"], "mf_minimization_iterations": toml_dict["mf_minimization_iterations"], "monte_carlo_iterations": toml_dict["monte_carlo_iterations"],
-                      "monte_carlo_minimization_iterations": toml_dict["monte_carlo_minimization_iterations"],
-                      "magnetic_fields_MHz": toml_dict["magnetic_fields_MHz"], "residues": toml_dict["residues"]}
-        fix_regex_pattern = "tau\d+|amp\d+"
-        parameters["fix"] = {k: val for k, val in toml_dict.items() if k not in parameters.keys() and re.match(fix_regex_pattern, k)}
+def read_parameter_file(param_file):
+    with open(param_file, "rb") as f:
+        toml_dict = tomli.load(f)
+    parameters = {"modes": toml_dict["modes"], "mf_minimization_iterations": toml_dict["mf_minimization_iterations"], "monte_carlo_iterations": toml_dict["monte_carlo_iterations"],
+                  "monte_carlo_minimization_iterations": toml_dict["monte_carlo_minimization_iterations"],
+                  "magnetic_fields_MHz": toml_dict["magnetic_fields_MHz"], "residues": toml_dict["residues"]}
+    fix_regex_pattern = "tau\d+|amp\d+"
+    parameters["fix"] = {k: val for k, val in toml_dict.items() if k not in parameters.keys() and re.match(fix_regex_pattern, k)}
     return parameters
 
 
