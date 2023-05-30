f7617d1415d455f1caa020afd1db340f7e72f541
Vincent Schnapka <133697445+VSchnapka@users.noreply.github.com>
2023-05-30 23:05:51 +0200

    Update constants_functions.py

--- modfree/constants_functions.py
+++ modfree/constants_functions.py
@@ -1,7 +1,7 @@
 # file containing the used constants and useful functions:
 import numpy as np
 
-theta = 22 # angle between CSA and dd axis
+# By default, theta = 22 # angle between CSA and dd axis
 
 gamma15N = -2.7126 * 1e7 # rad s-1 T-1
 gamma1H = 2.6752219 * 1e8 # rad s-1 T-1
@@ -12,7 +12,6 @@ h = 6.62607004e-34 # Planck constant
 R = 8.31446261815324 # J.mol-1.K-1
 
 d = mu_0*h*gamma1H*gamma15N/(8*(np.pi**2)*(rNH**3)) # NH dipolar coupling constant
-theta = theta*np.pi/180 # in rad.
 
 
 # Legendre 2
